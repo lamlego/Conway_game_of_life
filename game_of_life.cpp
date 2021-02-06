a28961a605d78f23e20120f291e02d417feb0f5f
TaylorFries <taylor.d.friesen@gmail.com>
2021-02-06 15:17:05 -0800

    Updating to actual working code

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sstream>
 
 //first run through of coding the game of life
 
@@ -19,19 +20,26 @@ int dead_or_alive(int living, int current){
     return new_int;
 }
 
-int main(int argc, char** argv){
+int main(int argc, char *argv[]){
     //maybe set it up so these variables can be input from user
-    int m = argv[1];
-    int n = argv[2];
-    int iterations = argv[3];
-    int world [m][n];
-    int new_world [m][n];
-
     if(argc > 4 || argc < 3){
-        
+            
         std::cout<<"Please enter 3 arguments";
         return 1;
     }
+
+    int m = atoi(argv[1]);
+    int n = atoi(argv[2]);
+    int iterations = atoi(argv[3]);
+    int world [m][n];
+    int new_world [m][n];
+
+    //Checking data that was entered making sure it is INT
+    if((m == 0) | (n == 0) | (iterations == 0)){
+        std::cout<< "must enter 3 INT values";
+        return 0;
+    }
+    
 
     //loading the matrix with "random" values
     for(int i = 0; i < m ; i++){
@@ -42,105 +50,105 @@ int main(int argc, char** argv){
     }
 
     //printing out the matrix
+    std::cout << "our *random* matrix\n";
     for (int i = 0; i < m ; i++){
         std::cout<<"\n";
         for(int j =0; j < n; j++){
             std::cout<<world[i][j]<<" ";
         }
     }
+    for(int iter = 0; iter < iterations; iter++){
+        std::cout<<"\n\n";
+        std::cout<< iter << "th iteration\n";
+        
+        //updating the matrix
+        int living = 0;
 
-    std::cout<<"\n\n";
-    
-    //updating the matrix
-    int living;
 
-    std::cout<<"living\n";
+        for (int i = 0; i < m ; i++){
+            for(int j = 0; j <n; j++){
 
-    for (int i = 0; i < m ; i++){
-        for(int j = 0; j <n; j++){
+                if (i ==0 ){
+                    //edge case 1
+                    if(j == 0){
+                        //edge case 1.1
+                        living = world[i+1][j] + world[i+1][j+1] + world[i][j+1];
+                        new_world[i][j] = dead_or_alive(living, world[i][j]);
+                        //std::cout<<living << " ";
+                        living = 0;
+                    }else if (j == 4){
+                        //edge case 1.2
+                        living = world[i][j-1] + world[i+1][j-1] + world[i+1][j];
+                        new_world[i][j] = dead_or_alive(living, world[i][j]);
+                        //std::cout<<living << " ";
+                        living = 0;
+                    }else{
+                        //base case 1
+                        living = world[i][j-1] + world[i+1][j-1] + world[i+1][j] + world[i+1][j+1] + world[i][j+1];
+                        new_world[i][j] = dead_or_alive(living, world[i][j]);
+                        //std::cout<<living << " ";
+                        living = 0;
+                    }
+                }else if (i == 4){
+                    //edge case 2
+                    if(j==0){
+                        //edge case 2.1
+                        living = world[i-1][j] + world[i-1][j+1] + world[i][j+1];
+                        new_world[i][j] = dead_or_alive(living, world[i][j]);
+                        //std::cout<<living << " ";
+                        living = 0;
+                    }else if(j == 4){
+                        //edge case 2.2
+                        living = world[i][j-1] + world[i-1][j-1] + world[i-1][j];
+                        new_world[i][j] = dead_or_alive(living, world[i][j]);
+                        //std::cout<<living << " ";
+                        living = 0;
+                    }else{
+                        //base case 2
+                        living = world[i][j-1] + world[i-1][j-1] + world[i-1][j] + world[i-1][j+1] + world[i][j+1];
+                        new_world[i][j] = dead_or_alive(living, world[i][j]);
+                        //std::cout<<living << " ";
+                        living = 0;
+                    }
+                }else if(j == 0){
+                    //edge case 3
+                    //we have already covered the cases where i = 0 and i = 4
+                    living = world[i-1][j] + world[i-1][j+1] + world[i][j+1] + world[i+1][j+1] + world[i+1][j];
+                    new_world[i][j] = dead_or_alive(living, world[i][j]);
+                    //std::cout<<living << " ";
+                    living = 0;
 
-            if (i ==0 ){
-                //edge case 1
-                if(j == 0){
-                    //edge case 1.1
-                    living = world[i+1][j] + world[i+1][j+1] + world[i][j+1];
-                    new_world[i][j] = dead_or_alive(living, world[i][j]);
-                    std::cout<<living << " ";
-                    living = 0;
-                }else if (j == 4){
-                    //edge case 1.2
-                    living = world[i][j-1] + world[i+1][j-1] + world[i+1][j];
-                    new_world[i][j] = dead_or_alive(living, world[i][j]);
-                    std::cout<<living << " ";
-                    living = 0;
-                }else{
-                    //base case 1
-                    living = world[i][j-1] + world[i+1][j-1] + world[i+1][j] + world[i+1][j+1] + world[i][j+1];
-                    new_world[i][j] = dead_or_alive(living, world[i][j]);
-                    std::cout<<living << " ";
-                    living = 0;
-                }
-            }else if (i == 4){
-                //edge case 2
-                if(j==0){
-                    //edge case 2.1
-                    living = world[i-1][j] + world[i-1][j+1] + world[i][j+1];
-                    new_world[i][j] = dead_or_alive(living, world[i][j]);
-                    std::cout<<living << " ";
-                    living = 0;
                 }else if(j == 4){
-                    //edge case 2.2
-                    living = world[i][j-1] + world[i-1][j-1] + world[i-1][j];
+                    //edge case 4
+                    //already covered i =0 and i=4
+                    living = world[i-1][j] + world[i-1][j-1] + world[i][j-1] + world[i+1][j-1] + world[i+1][j];
                     new_world[i][j] = dead_or_alive(living, world[i][j]);
-                    std::cout<<living << " ";
+                    //std::cout<<living << " ";
                     living = 0;
                 }else{
-                    //base case 2
-                    living = world[i][j-1] + world[i-1][j-1] + world[i-1][j] + world[i-1][j+1] + world[i][j+1];
+                    //base case
+                    //all the middle cell spots are going to be in this case. 
+                    living = world[i-1][j-1] + world[i-1][j] + world[i-1][j+1] + world[i][j+1] + world[i+1][j+1] + world[i+1][j] + world[i+1][j-1] + world[i][j-1];
                     new_world[i][j] = dead_or_alive(living, world[i][j]);
-                    std::cout<<living << " ";
+                    //std::cout<<living << " ";
                     living = 0;
                 }
-            }else if(j == 0){
-                //edge case 3
-                //we have already covered the cases where i = 0 and i = 4
-                living = world[i-1][j] + world[i-1][j+1] + world[i][j+1] + world[i+1][j+1] + world[i+1][j];
-                new_world[i][j] = dead_or_alive(living, world[i][j]);
-                std::cout<<living << " ";
-                living = 0;
-
-            }else if(j == 4){
-                //edge case 4
-                //already covered i =0 and i=4
-                living = world[i-1][j] + world[i-1][j-1] + world[i][j-1] + world[i+1][j-1] + world[i+1][j];
-                new_world[i][j] = dead_or_alive(living, world[i][j]);
-                std::cout<<living << " ";
-                living = 0;
-            }else{
-                //base case
-                //all the middle cell spots are going to be in this case. 
-                living = world[i-1][j-1] + world[i-1][j] + world[i-1][j+1] + world[i][j+1] + world[i+1][j+1] + world[i+1][j] + world[i+1][j-1] + world[i][j-1];
-                new_world[i][j] = dead_or_alive(living, world[i][j]);
-                std::cout<<living << " ";
-                living = 0;
             }
         }
-    std::cout<< '\n';
-    }
 
-    //updating the matrix
-    for(int i = 0; i<m; i++){
-        for(int j = 0; i<n; j++){
-            world[i][j] = new_world[i][j];
+        //updating the matrix
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                world[i][j] = new_world[i][j];
+            }
         }
-    }
 
-    
-    //printing out the matrix
-    for (int i = 0; i < m ; i++){
-        std::cout<<"\n";
-        for(int j =0; j < n; j++){
-            std::cout<<new_world[i][j]<<" ";
+        //printing out the matrix
+        for (int i = 0; i < m ; i++){
+            std::cout<<"\n";
+            for(int j =0; j < n; j++){
+                std::cout<<world[i][j]<<" ";
+            }
         }
     }
 

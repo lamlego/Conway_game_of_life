e959e5b5f74dd9b0b4bc24f98e5c7cca3cb6bad7
Ho Yin Lam <lamlego@g-cocoa.csc.uvic.ca>
2021-04-27 14:44:21 -0700

    simple vector addition

--- test.cu
+++ test.cu
@@ -29,59 +29,91 @@ bool* create_world(int m, int n){
     return world;
 }
 //takes a world and split it into 3 parts
-void primal_split(bool **world,bool **top, bool **bot,int m, int n){
-    m = m+2;
-    int count = m;
-    bool *t = *top;
-    bool *w = *world;
-    bool *b = *bot;
-    for(int i = 0; i< m*n; i++){
-        t[count] = w[i];
-        //cout<<w[i];
-        count++;
-    }
-    count = 0;
-    for(int i = m; i< m*n; i++){
+// void primal_split(bool **world,bool **top, bool **bot,int m, int n){
+//     m = m+2;
+//     int count = m;
+//     bool *t = *top;
+//     bool *w = *world;
+//     bool *b = *bot;
+//     for(int i = 0; i< m*n; i++){
+//         t[count] = w[i];
+//         //cout<<w[i];
+//         count++;
+//     }
+//     count = 0;
+//     for(int i = m; i< m*n; i++){
 
-        b[count] = w[i];
-        count++;
+//         b[count] = w[i];
+//         count++;
+//     }
+// }
+
+__global__
+void flip(bool *top, bool *core, bool *bot, int *res,int n){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i<n){
+        printf("hi %d\n",core[i]);
+            res[i] = top[i]+core[i]+bot[i];
+            
     }
+    
+
 }
 
 __global__
-void flip(bool *top, bool *core, bool *bot, int *res){
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    printf("hi %d\n",core[i]);
-    res[i] = top[i]+core[i]+bot[i];
-    
+void add(int *a,int *b,int *c,int n){
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    c[i]= a[i] +b[i];
 
 }
 
 int main(int argc, char *argv[]){
+    int size = 10* sizeof(int);
+    int *a, *b, *c;
+    int *d_a, *d_b, *d_c; 
+    a = (int *)malloc(size);
+    b = (int *)malloc(size);
+    c = (int *)malloc(size);   
+    for(int i=0;i<10;i++){
+        a[i]=i;
+        b[i]=i;
+    }
+    cout<<a[1];
+    cudaMalloc((void**)&d_a,size);
+    cudaMalloc((void**)&d_b,size);
+    cudaMalloc((void**)&d_c,size);
+
+    cudaMemcpy(d_a,a,size,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b,b,size,cudaMemcpyHostToDevice);
+    add<<<1,32>>>(d_a,d_b,d_c,10);
+    cudaMemcpy(c,d_c,size,cudaMemcpyDeviceToHost);
+
+    for(int i=0;i<10;i++){
+        cout<<c[i]<<" ";
+    }
     //cout<<sizeof(game_map)<<"\n";
-    bool *map = create_world(10,10);
-    bool *top,*bot;
-    bool *d_top, *d_core, *d_bot;
-    int *res, *d_res;
-    res = (int *)malloc(120*sizeof(int));
-    //top = (bool *)malloc(120*sizeof(bool));
-    top = new bool[120]();
-    bot = new bool[120]();
-    //bot = (bool *)malloc(120*sizeof(bool));
-    primal_split(&map,&top,&bot,10,10);
+    // bool *top,*bot;
+    // bool *d_top, *d_core, *d_bot;
+    // int *res, *d_res;
+    // res = (int *)malloc(120*sizeof(int));
+    // //top = (bool *)malloc(120*sizeof(bool));
+    // top = new bool[120]();
+    // bot = new bool[120]();
+    // //bot = (bool *)malloc(120*sizeof(bool));
+    // primal_split(&map,&top,&bot,10,10);
 
-    cudaMalloc((void **)&d_top,120*sizeof(bool));
-    cudaMalloc((void **)&d_core,120*sizeof(bool));
-    cudaMalloc((void **)&d_bot,120*sizeof(bool));
-    cudaMalloc((void **)&d_res,120*sizeof(int));
+    // cudaMalloc((void **)&d_top,120*sizeof(bool));
+    // cudaMalloc((void **)&d_core,120*sizeof(bool));
+    // cudaMalloc((void **)&d_bot,120*sizeof(bool));
+    // cudaMalloc((void **)&d_res,120*sizeof(int));
     
-    cudaMemcpy(d_top,top,120*sizeof(bool),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_core,map,120*sizeof(bool),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_bot,bot,120*sizeof(bool),cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_top,top,120*sizeof(bool),cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_core,map,120*sizeof(bool),cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_bot,bot,120*sizeof(bool),cudaMemcpyHostToDevice);
     
-    flip<<<10,12>>>(d_top,d_core,d_bot,d_res);
+    // flip<<<10,32>>>(d_top,d_core,d_bot,d_res,120);
 
-    cudaMemcpy(res,&d_res,120*sizeof(int),cudaMemcpyDeviceToHost);
+    // cudaMemcpy(res,&d_res,120*sizeof(int),cudaMemcpyDeviceToHost);
     // for(int i = 0;i<10;i++){
     //     for(int j =0;j<12;j++){
     //         cout<<top[i*12+j];
@@ -104,10 +136,10 @@ int main(int argc, char *argv[]){
     // }
     // cout<<"-------\n";
     //cout<<sizeof(map.core);
-    for(int i = 0;i<10;i++){
-        for(int j =0;j<12;j++){
-            cout<<res[i*12+j];
-        }
-        cout<< endl;
-    }
+    // for(int i = 0;i<10;i++){
+    //     for(int j =0;j<12;j++){
+    //         cout<<res[i*12+j];
+    //     }
+    //     cout<< endl;
+    // }
 }
